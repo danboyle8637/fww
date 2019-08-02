a4238c53b4f8c6550d6fb61cef32f29285b1e85c
Dan Boyle <danboyle8637@gmail.com>
2019-08-02 07:57:44 -0400

    Adding Firebase to project and getting this going with more site updates.

---
+++ src/firebase/fbConfig.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const configSetup = {
+  apiKey: "AIzaSyAHY3WBPdT26JVesJ0GFYTRhqOsQh_HDsU",
+  authDomain: "fit-womens-weekly.firebaseapp.com",
+  databaseURL: "https://fit-womens-weekly.firebaseio.com",
+  projectId: "fit-womens-weekly",
+  storageBucket: "",
+  messagingSenderId: "413346277257",
+  appId: "1:413346277257:web:c13b9270283d0118",
+};
+
+firebase.initializeApp(configSetup);
